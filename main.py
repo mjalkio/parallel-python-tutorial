bef78dc111bae6f05f3a75bbb79080dcb804872f
Michael Jalkio <mjalkio@gmail.com>
2018-03-19 21:45:44 -0700

    Small cleanup

--- main.py
+++ main.py
@@ -7,11 +7,9 @@ from dotenv import load_dotenv
 from facebook_client import FacebookClient
 
 load_dotenv()
-
 fb = FacebookClient(access_token=os.getenv('FACEBOOK_ACCESS_TOKEN'))
-
 nonprofit_df = pd.read_csv('nonprofit_facebook.csv')
 
 nonprofit_df['fan_count'] = nonprofit_df['facebook'].map(fb.get_page_fan_count)
 nonprofit_df['about'] = nonprofit_df['facebook'].map(fb.get_page_about)
-nonprofit_df.to_csv('output.csv')
+nonprofit_df.to_csv('output.csv', index=False)
